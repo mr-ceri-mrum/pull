9b4a2369535a19fca9bebd27a1297fd2d18048c7
makabake9 <zhunussoffilias0216@gmail.com>
2023-05-10 17:16:08 +0600

    new commit

---
+++ PollingSystem.ConsoleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PollingSystem.ConsoleClient;
+using PollingSystem.Entities;
+
+var builder = new PollBuilder("how are u")
+    .AddAnswer(1, "normaly")
+    .AddAnswer(2, "so so")
+    .AddAnswer(3, "good ")
+    .AddAnswer(4, "I am feel is cook");
+
+
+var poll = builder.Build();
+
+Console.WriteLine(builder.GetResults(poll));
+poll.VoteTo(1,5);
+poll.VoteTo(3);
+poll.VoteTo(4);
+poll.VoteTo(3);
+
+
+var result = builder.GetResults(poll);
+Console.WriteLine(result.GetView());
